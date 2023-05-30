5d6a6d5c3de62fa165489bb075c38cee75400e65
Tompa123 <mrguy_93@hotmail.com>
2023-05-30 09:26:55 +0200

    webshopitem added

---
+++ webshopitem/src/test/java/com/example/webshopitem/WebshopitemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopitemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
