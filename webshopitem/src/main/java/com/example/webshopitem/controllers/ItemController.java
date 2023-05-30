614eca4af12f283e112097b39933ec5f010adb4f
Tompa123 <mrguy_93@hotmail.com>
2023-05-30 12:15:40 +0200

    item finished

--- webshopitem/src/main/java/com/example/webshopitem/controllers/ItemController.java
+++ webshopitem/src/main/java/com/example/webshopitem/controllers/ItemController.java
@@ -1,6 +1,8 @@
 package com.example.webshopitem.controllers;
 import com.example.webshopitem.models.Item;
 import com.example.webshopitem.repositories.ItemRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import java.util.List;
 
 
 @RestController
+@Tag(name = "Item Controller", description = "Item Controller API, used to create, get and list items")
 public class ItemController {
 
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
@@ -20,12 +23,14 @@ public class ItemController {
         this.itemRepository = itemRepository;
     }
 
-    @GetMapping({"/items", "/items/"})
+    @GetMapping("/items")
+    @Operation(summary = "Get all items", description = "Get all items from the database")
     public List<Item> getAllItems() {
         return itemRepository.findAll();
     }
 
-    @PostMapping({"/items", "/items/"})
+    @PostMapping("/items")
+    @Operation(summary = "Create an item", description = "Create an item in the database")
     public ResponseEntity<Object> createItem(@RequestBody Item item) {
         if (item == null) {
             return new ResponseEntity<>(Collections.singletonMap("error", "Item not found"), HttpStatus.NOT_FOUND);
@@ -40,7 +45,8 @@ public class ItemController {
         }
     }
 
-    @RequestMapping("/items/{id}")
+    @GetMapping("/items/{id}")
+    @Operation(summary = "Get an item by id", description = "Get an item by id from the database")
     public ResponseEntity<Object> getItemById(@PathVariable(required = false) long id) {
         Item item = itemRepository.findById(id).isPresent() ? itemRepository.findById(id).get() : null;
         if (item == null) {
