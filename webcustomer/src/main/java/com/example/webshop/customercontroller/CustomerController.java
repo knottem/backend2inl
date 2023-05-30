d60b5bd1d1d4e92d22b7135966a3a90991ea6f8e
Sevrin94 <115183505+Sevrin94@users.noreply.github.com>
2023-05-30 13:16:09 +0200

    New new

--- webcustomer/src/main/java/com/example/webshop/customercontroller/CustomerController.java
+++ webcustomer/src/main/java/com/example/webshop/customercontroller/CustomerController.java
@@ -2,6 +2,8 @@ package com.example.webshop.customercontroller;
 
 import com.example.webshop.repository.CustomerRepository;
 import com.example.webshop.model.Customer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
+@Tag(name="Customer controller", description= "Get customerAPI, get all customers, get customers by ID" )
 public class CustomerController {
     private final CustomerRepository customerRepository;
     public CustomerController(CustomerRepository repo) {
@@ -17,11 +20,13 @@ public class CustomerController {
     }
 
     @GetMapping("/customers")
+    @Operation(summary= "Get customers", description = "Get all customers from list" )
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
 
     @GetMapping("/customers/{id}")
+    @Operation(summary= "Get customers by ID", description = "Use ID to get the customers")
     public ResponseEntity<Object> getCustomerById(@PathVariable long id) {
         Customer customer = customerRepository.findById(id).isPresent() ? customerRepository.findById(id).get() : null;
         if (customer == null) {
@@ -32,6 +37,7 @@ public class CustomerController {
     }
 
     @PostMapping({"/customers", "/customers/"})
+    @Operation(summary = "Create customer", description = "Here you create a new customer using personal data")
     public ResponseEntity<Object> createCustomer(@RequestBody Customer customer) {
         if (customer == null) {
             return new ResponseEntity<>(Collections.singletonMap("error", "Customer not found"), HttpStatus.NOT_FOUND);
@@ -49,6 +55,7 @@ public class CustomerController {
         }
     }
     @GetMapping("/customersbool/{id}")
+    @Operation(summary= "Get customer bool", description = "Returns true or false")
     public ResponseEntity<Object> getCustomerBool(@PathVariable long id) {
         boolean customerExists = customerRepository.findById(id).isPresent();
         if (!customerExists) {
