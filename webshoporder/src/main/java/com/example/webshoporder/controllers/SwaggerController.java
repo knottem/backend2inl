afbf184ba283cdedad0bc6b681a7812e59909f75
knottem <59744487+knottem@users.noreply.github.com>
2023-05-31 11:34:00 +0200

    Added notes to classes

--- webshoporder/src/main/java/com/example/webshoporder/controllers/SwaggerController.java
+++ webshoporder/src/main/java/com/example/webshoporder/controllers/SwaggerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SwaggerController {
 
+    // Redirects to Swagger UI, so that the user can easily test the API
     @GetMapping("/")
     public String index() {
         return "redirect:/swagger-ui/index.html";
